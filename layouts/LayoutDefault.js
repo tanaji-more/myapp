4ba760aed0bee479b37a4b6a98820306c26117c5
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-05-30 20:48:47 +0530

    Add files via upload

--- layouts/LayoutDefault.js
+++ layouts/LayoutDefault.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const LayoutDefault = ({ children }) => {
+  return (
+    <div>
+      <header>Header</header>
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default LayoutDefault;
