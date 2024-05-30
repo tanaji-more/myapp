05adbea5feff5b2506a706dd97be650db169b727
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-05-30 20:45:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './App.css'; // assuming you have a CSS file for global styles
+import BusinessComponent from './components/BusinessComponent';
+import ImageTemplatesComponent from './components/ImageTemplatesComponent';
 
+function App() {
+  return (
+    <div className="App">
+      <BusinessComponent />
+      <ImageTemplatesComponent />
+    </div>
+  );
+}
+
+export default App;
