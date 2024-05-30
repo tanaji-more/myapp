27f589294171585b63c55b5d71eb8f487413646e
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-05-30 20:47:42 +0530

    Add files via upload

--- views/Home.js
+++ views/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of your application.</p>
+    </div>
+  );
+};
+
+export default Home;
