02dc130815564a411f6c429dbfce4a128f6606f5
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-05-30 20:49:36 +0530

    Add files via upload

--- utils/AppRoute.js
+++ utils/AppRoute.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
 
+const AppRoute = ({ component: Component, layout: Layout, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      element={
+        <Layout>
+          <Component />
+        </Layout>
+      }
+    />
+  );
+};
+
+export default AppRoute;
